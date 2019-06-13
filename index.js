e7de46cba06dcd19ee784905ff2b0c8b285b953c
Jay Chen <jay.chen@hbo.com>
2019-06-13 16:11:23 +0800

    init

---
+++ index.js
@@ -0,0 +1,15 @@
+'use strict'
+
+const fs = require('fs')
+const validate = require('./lib/validate')
+const getResults = require('./lib/getResults')
+const { getAPI, getOrAddRun, updateResults } = require('./lib/testrail')
+
+module.exports = async function postResults (options) {
+  validate(options)
+  const data = JSON.parse(fs.readFileSync(options.source))
+  const results = getResults(data, options.prefix, options.passId, options.failId)
+  const api = getAPI(options.url, options.user, options.token)
+  const runId = await getOrAddRun(api, options.projectId, options.suiteId)
+  return updateResults(api, runId, results)
+}
